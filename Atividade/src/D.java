14d6c5c6a33ab0d5671c94a721a062a843e28897
Lucas Bicalho <lucas.bicalho1@estudante.ufla.br>
2023-10-01 16:29:49 -0300

    Criação da D e alteração da C

---
+++ Atividade/src/D.java
@@ -0,0 +1,12 @@
+public class D {
+    private int D1;
+    private float D2;
+
+    public int MD1() {
+        return D1;
+    }
+
+    public void MD2() {
+        System.out.println("MD2");
+    }
+}
